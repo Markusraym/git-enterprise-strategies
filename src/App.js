c0c865f18c57df87e1d4dc34dc87ba386fd4f053
samilieberman <samilieberman1@gmail.com>
2022-11-07 20:54:51 -0500

    added new project text

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import gatorLogo from './gator.gif';
+// Change the import to match the variable name 'logo' and file used by newtext
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={gatorLogo} className="App-logo" alt="logo" />
-        <p> Hello World! </p>
-        <p> My name is Mark Ploski and this is my project </p>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello World! This is our team project.</p>
+        <p>My name is Mark Ploski and this is my project contribution</p>
       </header>
     </div>
   );
